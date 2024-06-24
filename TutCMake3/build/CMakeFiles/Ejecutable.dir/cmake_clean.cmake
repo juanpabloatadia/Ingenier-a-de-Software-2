d14fba9a10c74355c1c3d45e763e967953435d38
juanpabloatadia <101681412+juanpabloatadia@users.noreply.github.com>
2024-06-23 22:26:58 -0300

    Add files via upload

---
+++ TutCMake3/build/CMakeFiles/Ejecutable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejecutable.dir/main.cpp.o"
+  "Ejecutable.pdb"
+  "Ejecutable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TutCMake3/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, Gaby! \n";
+	return 0;
+}

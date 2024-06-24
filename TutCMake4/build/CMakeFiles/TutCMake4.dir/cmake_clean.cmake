f6f588f3dad3d3fab703c1feee13f6aff647b2c3
juanpabloatadia <101681412+juanpabloatadia@users.noreply.github.com>
2024-06-23 22:27:50 -0300

    Add files via upload

---
+++ TutCMake4/build/CMakeFiles/TutCMake4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TutCMake4.dir/main.cpp.o"
+  "TutCMake4.pdb"
+  "TutCMake4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TutCMake4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TutCMake4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int foo() {}
+
+int main()
+{
+	std::cout << "Hello, Gaby! \n";
+	return 0;
+}
+
+
